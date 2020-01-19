3b59f941059dac78ad9d7f5fef52ad69e53088e3
Kohei Otsuka <kochizufan@gmail.com>
2020-01-20 08:41:40 +0900

    ある程度整理

--- js/config.js
+++ js/config.js
@@ -8,16 +8,12 @@ requirejs.config({
     paths: {
         'ol3': '../common/lib/ol-maplat',
         'bootstrap': '../common/lib/bootstrap-native',
-        'backbone': 'backbone-min',
         'underscore': 'underscore-min',
-        'jquery': 'dummy-jq', // Dummy
         'histmap': '../common/js/histmap',
-        'aigle': '../common/js/aigle-es5.min',
         'ol-custom': '../common/js/ol-custom',
         'histmap_tin': '../common/js/histmap_tin',
         'tin': '../common/js/tin',
         'turf': '../common/lib/turf_maplat.min',
-        //'mapshaper': '../node_modules/mapshaper/mapshaper',
         'mapshaper': '../common/lib/mapshaper_maplat',
         'resize': '../common/js/detect-element-resize',
         'contextmenu': 'ol3-contextmenu',
@@ -31,9 +27,6 @@ requirejs.config({
         },
         'turf': {
             exports: 'turf'
-        },
-        'aigle': {
-            exports: 'Promise'
         },
         'contextmenu': {
             deps: ['ol3']
