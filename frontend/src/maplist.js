91a5e1013a3e2fe74ab2d297f90b02f81d7e3d67
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-01-22 05:23:09 +0900

    maplistのfrontendをES6化してみたが動かない

---
+++ frontend/src/maplist.js
@@ -0,0 +1,48 @@
+import Wookmark from 'wookmark/wookmark';
+import Vue from 'vue';
+
+const {ipcRenderer} = require('electron'); // eslint-disable-line no-undef
+
+const app = new Vue({ // eslint-disable-line no-unused-vars
+    created() {
+        let wookmark; // eslint-disable-line no-unused-vars
+        const self = this;
+
+        const backend = require('electron').remote.require('./maplist'); // eslint-disable-line no-undef
+        backend.request();
+
+        ipcRenderer.on('mapListAdd', (event, arg) => {
+            const map = {
+                mapID: arg.mapID,
+                name: arg.title,
+            };
+            if (!arg.width || !arg.height || !arg.thumbnail) {
+                map.height = 190;
+                map.image = 'img/no_image.png';
+            } else {
+                map.height = Math.floor(190 / arg.width * arg.height);
+                map.image = arg.thumbnail;
+            }
+
+            self.maplist.push(map);
+            self.maplist = self.maplist.sort((a, b) => {
+                if (a.mapID < b.mapID) return -1;
+                if (a.mapID > b.mapID) return 1;
+                // a は b と等しいはず
+                return 0;
+            });
+
+            Vue.nextTick(() => {
+                wookmark = new Wookmark('#maplist');
+            });
+        });
+        /*Vue.nextTick(() => {
+            wookmark = new Wookmark('#maplist');
+        });*/
+    },
+    el: '#maplist',
+    data: {
+        maplist: []
+    }
+});
+//    });
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const pjson = require('./package.json');
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: {
+        maplist: './frontend/src/maplist.js'
+    },
+    output: {
+        path: `${__dirname}/frontend/dist`,
+        filename: '[name].bundle.js'
+    },
+    node: {
+        electron: "empty"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        "presets": [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    "corejs": 3
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    /*plugins: [
+        new webpack.BannerPlugin({
+            banner: `${pjson.name} v${pjson.version} | ${pjson.author} | license: ${pjson.license}`
+        })
+    ]*/
+    externals: [
+        (function () {
+            var IGNORES = [
+                'electron'
+            ];
+            return function (context, request, callback) {
+                if (IGNORES.indexOf(request) >= 0) {
+                    return callback(null, "require('" + request + "')");
+                }
+                return callback();
+            };
+        })()
+    ]
+};
