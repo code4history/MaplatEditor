869a4038df1dc565876f686e11613a7d4734ee68
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-05-24 10:41:51 +0900

    backend/settings 作り直し中

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const EventEmitter = require('events'); // eslint-disable-line no-undef
 const storage = require('electron-json-storage'); // eslint-disable-line no-undef
 const path = require('path'); // eslint-disable-line no-undef
 const app = require('electron').app; // eslint-disable-line no-undef
@@ -8,6 +9,8 @@ const electron = require('electron'); // eslint-disable-line no-undef
 const BrowserWindow = electron.BrowserWindow;
 const tmsList = require('../../tms_list.json'); // eslint-disable-line no-undef
 let json;
+let undoredo;
+let settings;
 
 const protect = [
     'tmpFolder',
@@ -20,6 +23,9 @@ const defaultSetting = {
 
 const settings = {
     init() {
+        if (!undoredo) {
+            undoredo = new UndoRedo();
+        }
         if (json) return;
         storage.getAll((error, data) => {
             if (error) throw error;
@@ -60,8 +66,112 @@ const settings = {
                 focused.webContents.send('saveFolderSelected',baseDir[0]);
             }
         });
+    },
+    do(verb, data) {
+        undoredo.do(verb, data);
+    },
+    undo() {
+        undoredo.undo();
+    },
+    redo() {
+        undoredo.redo();
     }
 };
 settings.init();
+
+class Settings extends EventEmitter {
+    static init() {
+        if (!settings) {
+            settings = new Settings();
+        }
+        return settings;
+    }
+
+    constructor() {
+        super();
+        this.behaviorChain = [];
+        this.currentPosition = 0;
+        storage.getAll((error, data) => {
+            if (error) throw error;
+
+            if (Object.keys(data).length === 0) {
+                this.json = {
+                    saveFolder: path.resolve(app.getPath('documents') + path.sep + app.getName())
+                };
+                storage.set('saveFolder', this.json.saveFolder, {});
+                fs.ensureDir(this.json.saveFolder, () => {});
+            } else {
+                this.json = data;
+            }
+            this.json = Object.assign(defaultSetting, this.json);
+            this.json.tmpFolder = path.resolve(app.getPath('temp') + path.sep + app.getName());
+            fs.ensureDir(this.json.tmpFolder, () => {});
+            this.json.tmsList = tmsList;
+        });
+    }
+
+    do(verb, data) {
+        if (this.redoable) {
+            this.behaviorChain = this.behaviorChain.slice(0, this.currentPosition + 1);
+        }
+        this.behaviorChain.push({verb, data});
+        this.currentPosition++;
+    }
+
+    get redoable() {
+        return this.behaviorChain.length != this.currentPosition;
+    }
+
+    redo() {
+        if (!this.redoable) return;
+        this.currentPosition++;
+        return this.behaviorChain[this.currentPosition].data;
+    }
+
+    get undoable() {
+        return this.currentPosition != 0;
+    }
+
+    undo() {
+        if (!this.undoable) return;
+        this.currentPosition--;
+        return this.behaviorChain[this.currentPosition].data;
+    }
+
+    get menuData() {
+        return {
+            undoable: this.undoable,
+            redoable: this.redoable,
+            undo: this.undoable ? this.behaviorChain[this.currentPosition - 1].verb : undefined,
+            redo: this.redoable ? this.behaviorChain[this.currentPosition].verb : undefined
+        };
+    }
+
+    getSetting(key) {
+        return this.json[key];
+    }
+
+    getSettings() {
+        return this.json;
+    }
+
+    setSetting(key, value) {
+        if (protect.indexOf(key) >= 0) throw `"${key}" is protected.`;
+        this.json[key] = value;
+        storage.set(key, value, {}, (error) => {
+            if (error) throw error;
+        });
+    }
+
+    showSaveFolderDialog(oldSetting) {
+        const dialog = require('electron').dialog; // eslint-disable-line no-undef
+        const focused = BrowserWindow.getFocusedWindow();
+        dialog.showOpenDialog({ defaultPath: oldSetting, properties: ['openDirectory']}, (baseDir) => {
+            if(baseDir && baseDir[0]) {
+                focused.webContents.send('saveFolderSelected',baseDir[0]);
+            }
+        });
+    }
+}
 
 module.exports = settings; // eslint-disable-line no-undef
